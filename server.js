ac6696c0dcf09b7ae3e309dfe9b1702b166aeb33
Archit <jainarchit1604@gmail.com>
2024-01-28 16:20:13 +0530

    Fourth

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const port = 3000;
+const path = require("path");
+const port = 10000;
 
 // Serve files from the current directory
 app.use(express.static(__dirname));
